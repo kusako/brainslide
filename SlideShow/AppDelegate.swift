c961fd0f24e55f0681516e4fc5137520b3b29e8c
Markus Strickler <markus.strickler@publicispixelpark.de>
2017-01-19 14:17:55 +0100

    Initial Commit

---
+++ SlideShow/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SlideShow
+//
+//  Created by Markus Strickler on 19.01.17.
+//  Copyright © 2017 Markus Strickler. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SlideShow/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SlideShow
+//
+//  Created by Markus Strickler on 19.01.17.
+//  Copyright © 2017 Markus Strickler. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
